3acd1a4f2d9043e6ec505e2794c3d4a7b9a3cfac
송 종근 <carrotcarrot@Baepeu.local>
2019-06-07 15:45:02 +0900

    comment delete

--- comment/urls.py
+++ comment/urls.py
@@ -2,4 +2,5 @@ from django.urls import path
 from .views import *
 urlpatterns = [
     path('add_comment/', add_comment, name='add_comment'),
+    path('delete_comment/<int:pk>/', delete_comment, name='delete_comment'),
 ]
--- comment/views.py
+++ comment/views.py
@@ -1,22 +1,43 @@
 from django.shortcuts import render
 
 from .forms import CommentForm
+from .models import Comment
 from django.shortcuts import redirect
 from django.urls import resolve
 from urllib.parse import urlparse
 
+from django.contrib import messages
 def add_comment(request):
-    comment_form = CommentForm(request.POST)
-    comment_form.instance.author_id = request.user.id
-    if comment_form.is_valid():
-        comment_form.save()
+    if not request.user.is_anonymous:
+        comment_form = CommentForm(request.POST)
+        comment_form.instance.author_id = request.user.id
+        if comment_form.is_valid():
+            comment_form.save()
+            messages.add_message(request, messages.SUCCESS, "댓글을 작성하였습니다.")
+        else:
+            messages.add_message(request, messages.WARNING, "Comment Invalid")
     else:
-        print('form invalid')
+        messages.add_message(request, messages.WARNING, "댓글은 로그인 사용자만 남길 수 있습니다.")
+
     referer = request.META['HTTP_REFERER']
     return redirect(referer)
 
-    #print(comment_form.instance.__dict__)
-    # print(referer)
-    # url = urlparse(referer)
-    # url = resolve(url.path)
-    # print("url",url)
+def delete_comment(request, pk):
+    comment = Comment.objects.filter(pk=pk)
+    if comment.exists() and comment[0].author == request.user :
+        comment.delete()
+        messages.add_message(request, messages.SUCCESS, "댓글을 삭제하였습니다.")
+    else:
+        messages.add_message(request, messages.WARNING, "댓글을 삭제할 수 없습니다.")
+
+    referer = request.META['HTTP_REFERER']
+    return redirect(referer)
+
+
+
+
+
+
+
+
+

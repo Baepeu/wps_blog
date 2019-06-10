1918a38907998b194e721ae7278fd02f68f96630
송 종근 <carrotcarrot@Baepeu.local>
2019-06-10 10:22:28 +0900

    change live settings

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '*-u0wmbgbqel!9y8&f#=04397+2t-%+_bm*$lt2(53)%q((ihs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
